0ea6a4a20b309dfbf8c2cee5dca2081c65833293
AoneAbe <abe@aone-soft.co.jp>
2025-04-10 04:04:42 +0900

    RouteHandler最新版2

--- src/app/api/memories/[id]/route.ts
+++ src/app/api/memories/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/lib/auth';
 import prisma from '@/lib/prisma';
-import { Route } from 'next';
+// import { Route } from 'next';
 
 type ImageDataInput = {
   id?: string;
